6da6e73dcccedb57ab03b7333bdf8becffc4f649
codevarty <hisu7789@gmail.com>
2023-04-19 01:11:23 +0900

    react-app 폴더 내의 App.js 파일 업데이트

--- react-app/my-app/src/App.js
+++ react-app/my-app/src/App.js
@@ -1,11 +1,17 @@
 import Button from './Button';
 import styles from './App.module.css';
+import { useState } from "react";
 
 function App() {
+  // state가 변경 될 때 마다 컴포넌트가 리렌더링 된다.
+  // 즉 이 함수가 실행되는 것이다.
+  const [count, setCount] = useState(0);
+  const onClick= () => setCount((prev) => prev + 1);
+  // console.log("render"); //state가 변경될 때 마다 실행된다.
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <h1>{count}</h1>
+      <Button onClick={onClick} text={"Click me"} />
     </div>
   );
 }
