dd1d1d93c3fdfa0b2ca3a54148d9fada8cf171a2
codevarty <hisu7789@gmail.com>
2023-04-20 00:14:02 +0900

    파일 업데이트

--- react-app/my-app/src/App.js
+++ react-app/my-app/src/App.js
@@ -1,17 +1,20 @@
-import Button from './Button';
-import styles from './App.module.css';
-import { useState } from "react";
+// import Button from './Button';
+// import styles from './App.module.css';
+import { useState, useEffect} from "react";
 
 function App() {
   // state가 변경 될 때 마다 컴포넌트가 리렌더링 된다.
   // 즉 이 함수가 실행되는 것이다.
   const [count, setCount] = useState(0);
   const onClick= () => setCount((prev) => prev + 1);
-  // console.log("render"); //state가 변경될 때 마다 실행된다.
+  console.log('I run all the time') // 버튼을 클릭할 때 마다 실행
+  useEffect(() => {
+    console.log('I run only once') // 한 번만 실행
+  },[]);
   return (
     <div>
       <h1>{count}</h1>
-      <Button onClick={onClick} text={"Click me"} />
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
--- react-app/my-app/src/index.js
+++ react-app/my-app/src/index.js
@@ -5,8 +5,6 @@ import App from './App';
 // react 18 이후부터 createRoot를 사용해야 한다.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
