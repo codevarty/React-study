0a325cbb2b7877f956253affe70cf21816ba9a17
codevarty <hisu7789@gmail.com>
2023-05-01 15:49:37 +0900

    movie app 파일 업데이트

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,8 +1,25 @@
-
+import {
+  //HashRouter는 브라우저 주소뒤에 /#/가 붙는다.
+  //BrowserRouter는 브라우저 주소뒤에 /#/가 붙지 않는다.
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 /**Movie App */
 // map 함수는 새로운 배열을 반환한다.
 function App() {
-  return null;
+  return <Router>
+    <Switch>
+      <Route path="/movie">
+        <Detail/>
+      </Route>
+      <Route /*Home Route*/ path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
 }
 
 export default App;
--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types"
+//Link는 브라우저 새로고침이 없어도 페이지 이동이 가능하게 해주는 컴포넌트이다.
+import {Link} from "react-router-dom"
 
 function Movie({coverImg, title, summary, genres}) {
   return <div>
   <img src={coverImg} alt={title} />
-  <h2>{title}</h2>
+  <h2><Link to="/movie">{title}</Link></h2>
   <p>{summary}</p>
   <ul>
     {genres && genres.map((g) => 
